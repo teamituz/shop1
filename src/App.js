61f31c358423be8bc1b6791be946b70c29252954
Shohjaxon <karol6727@gmail.com>
2022-10-15 16:19:29 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./pages/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Header />
+      <Header />
+      
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <>
+      <div>Header</div>
+      <div>Header</div>
+      <div>Header</div>
+    </>
+  );
+}
+
+export default Header;
